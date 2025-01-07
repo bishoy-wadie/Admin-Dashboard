0ce1cf67cad7a7985dd98c01a76c56c3050f237d
bishoy-wadie <bishoy.o.wadie@gmail.com>
2025-01-07 16:16:43 +0200

    declare notfound component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,9 +4,10 @@ import { SidebarComponent } from './components/sidebar/sidebar.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { TableComponent } from './components/table/table.component';
 import { RouterModule } from '@angular/router';
+import { NotFoundComponent } from './components/not-found/not-found.component';
 
 @NgModule({
-  declarations: [SidebarComponent, NavbarComponent, TableComponent],
+  declarations: [SidebarComponent, NavbarComponent, TableComponent, NotFoundComponent],
   imports: [CommonModule, RouterModule],
   exports: [NavbarComponent, SidebarComponent, TableComponent],
 })
