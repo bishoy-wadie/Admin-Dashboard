1f5fb659826e6f475ba7988993e7c185250b2f8d
bishoy-wadie <bishoy.o.wadie@gmail.com>
2025-01-08 21:37:30 +0200

    get all categories

--- src/app/categories/categories.module.ts
+++ src/app/categories/categories.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CategoriesRoutingModule } from './categories-routing.module';
-import { CategoriesComponent } from './categories.component';
+import { CategoriesComponent } from './components/categoires/categories.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [CategoriesComponent],
-  imports: [CommonModule, CategoriesRoutingModule],
+  imports: [CommonModule, CategoriesRoutingModule, SharedModule],
 })
 export class CategoriesModule {}
