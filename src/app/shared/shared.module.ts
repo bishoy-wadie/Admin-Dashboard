72f7a7c42394d4f367fbba77c8dddd7c940ae3f0
bishoy-wadie <bishoy.o.wadie@gmail.com>
2025-01-08 14:11:43 +0200

    export new shared components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,10 +5,25 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { TableComponent } from './components/table/table.component';
 import { RouterModule } from '@angular/router';
 import { NotFoundComponent } from './components/not-found/not-found.component';
+import { TableSkeletonComponent } from './components/table-skeleton/table-skeleton.component';
+import { SearchComponent } from './components/search/search.component';
 
 @NgModule({
-  declarations: [SidebarComponent, NavbarComponent, TableComponent, NotFoundComponent],
+  declarations: [
+    SidebarComponent,
+    NavbarComponent,
+    TableComponent,
+    NotFoundComponent,
+    TableSkeletonComponent,
+    SearchComponent,
+  ],
   imports: [CommonModule, RouterModule],
-  exports: [NavbarComponent, SidebarComponent, TableComponent],
+  exports: [
+    NavbarComponent,
+    SidebarComponent,
+    TableComponent,
+    TableSkeletonComponent,
+    SearchComponent,
+  ],
 })
 export class SharedModule {}
